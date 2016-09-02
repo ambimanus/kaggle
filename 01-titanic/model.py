b9790b7167bfb679040d9cf15cf65a3fccdf0a87
Christian Hinrichs <christian.hinrichs@ambimanus.com>
2016-09-02 13:33:58 +0200

    refactoring, TODO notes

--- 01-titanic/model.py
+++ 01-titanic/model.py
@@ -6,8 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 
 def read_and_clean(filename):
     df = pd.read_csv(filename, header=0)
-    # Drop non-numeric, non-categorical data
-    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
     # Embarked: Fill NaN with most common value (here: 'S')
     embarked_fill = df['Embarked'].value_counts().index[0]
     df['Embarked'] = df['Embarked'].fillna(embarked_fill)
@@ -16,6 +14,7 @@ def read_and_clean(filename):
     # Same for Sex:
     df['Sex'] = df['Sex'].map({'female': 0, 'male': 1}).astype(int)
     # Age: Fill NaN with median age in each PClass with respect to Sex:
+    # TODO: Include title information from Name as indicator for child/grown-up
     for sex in range(2):
         for pclass in range(1, 4):
             df.loc[(df['Age'].isnull()) &
@@ -24,6 +23,7 @@ def read_and_clean(filename):
                 df[(df['Sex'] == sex) &
                    (df['Pclass'] == pclass)]['Age'].dropna().median()
     # Similar for Fare, but also including Embarked:
+    # TODO: Should also include binned age
     for sex in range(2):
         for pclass in range(1, 4):
             for embarked in range(3):
@@ -34,31 +34,32 @@ def read_and_clean(filename):
                     df[(df['Sex'] == sex) &
                        (df['Pclass'] == pclass) &
                        (df['Embarked'] == embarked)]['Fare'].dropna().median()
+    # TODO: Convert Age to categorical values
     # print df['Age'].value_counts(dropna=False)
     # print df['Age'].value_counts(bins=10, sort=False)
+    # TODO: Create categorical 'Title' variable derived from Name
+    # Drop non-numeric, non-categorical data
+    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
     # Extract passenger ids
     ids = df['PassengerId'].values
     df = df.drop('PassengerId', axis=1)
     return df, ids
 
 
-def build_forest(data):
-    forest = RandomForestClassifier(n_estimators=100)
-    return forest.fit(data[:, 1:], data[:, 0])
-
-
 if __name__ == '__main__':
     # Training data
     df_train, _ = read_and_clean('train.csv')
     df_train.info()
-    forest = build_forest(df_train.values)
+    train = df_train.values
+    forest = RandomForestClassifier(n_estimators=100)
+    fit = forest.fit(train[:, 1:], train[:, 0])
 
     # Test data
     df_test, ids = read_and_clean('test.csv')
     df_test.info()
 
     # Prediction
-    prediction = np.array(forest.predict(df_test.values), dtype=int)
+    prediction = np.array(fit.predict(df_test.values), dtype=int)
     out = np.stack((np.array(ids, dtype=int), prediction), axis=-1)
     np.savetxt('prediction.csv', out, fmt='%d', delimiter=',',
                header='PassengerId,Survived', comments='')
